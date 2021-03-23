0e169b6c255b6d3775942297aba8581488ca64b0
begai <Begaiasanbekova@gmail.com>
2021-03-23 22:16:10 +0100

    fix slider

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,25 +1,45 @@
 const slider = () => {
-    const topSlider = document.querySelectorAll('.top-slider'),
-        slide = document.querySelectorAll('.item'),
-        dot = document.querySelector('.dot');
+    const topSlider = document.querySelector('.top-slider'),
+        slide = document.querySelectorAll('.item');
 
-    let indexValue = 0;
+    let indexValue = 0,
+        interval;
     const showSlider = () => {
-        setTimeout(showSlider, 2000);
         for (let i = 0; i < slide.length; i++) {
             slide[i].style.display = 'none';
         }
+
         indexValue++;
 
         if (indexValue > slide.length) {
             indexValue = 1;
         }
         slide[indexValue - 1].style.display = 'block';
-
     }
-    showSlider();
 
+    const startSlider = () => {
+        interval = setInterval(showSlider, 2000);
+    };
 
+    const stopSlide = () => {
+        clearInterval(interval);
+    };
+
+    startSlider();
+
+    topSlider.addEventListener('mouseover', event => {
+        let target = event.target;
+        if (target.matches('.item')) {
+            stopSlide();
+        }
+    });
+
+    topSlider.addEventListener('mouseout', event => {
+        let target = event.target;
+        if (target.matches('.item')) {
+            startSlider();
+        }
+    });
 }
 
 
